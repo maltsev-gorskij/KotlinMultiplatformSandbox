c64ef5e4024f4883d7c17933143d0178e5448b53
Dmitry Maltsev-Gorskij <maltsev-gorskij@yandex.ru>
2023-01-19 16:52:10 +0300

    - Implemented simple example of compose navigation

---
+++ androidApp/src/main/java/ru/lyrian/kotlinmultiplatformsandbox/android/core/ui/navigation/NavigationRoot.kt
@@ -0,0 +1,25 @@
+package ru.lyrian.kotlinmultiplatformsandbox.android.core.ui.navigation
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import ru.lyrian.kotlinmultiplatformsandbox.android.feature.spaceXLaunches.presentation.ui.SpaceXLaunchesScreen
+
+@Composable
+fun NavigationRoot() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = NavigationDestinations.LAUNCHES_LIST.route) {
+        composable(NavigationDestinations.LAUNCHES_LIST.route) { SpaceXLaunchesScreen(navController) }
+        composable(NavigationDestinations.LAUNCHES_DETAILS.route) {
+            Surface(modifier = Modifier.fillMaxSize()) {
+                Text("Just a favorites placeholder")
+            }
+        }
+    }
+}
