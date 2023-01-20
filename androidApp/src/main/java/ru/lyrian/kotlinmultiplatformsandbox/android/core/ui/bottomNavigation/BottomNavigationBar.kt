d4a9cd752ef0a291ae6f5ae81a30cad5766fc1b9
Dmitry Maltsev-Gorskij <maltsev-gorskij@yandex.ru>
2023-01-20 20:43:06 +0300

    - Added Animated visibility to BottomNavigationBar

--- androidApp/src/main/java/ru/lyrian/kotlinmultiplatformsandbox/android/core/ui/bottomNavigation/BottomNavigationBar.kt
+++ androidApp/src/main/java/ru/lyrian/kotlinmultiplatformsandbox/android/core/ui/bottomNavigation/BottomNavigationBar.kt
@@ -1,11 +1,18 @@
 package ru.lyrian.kotlinmultiplatformsandbox.android.core.ui.bottomNavigation
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -20,8 +27,17 @@ fun BottomNavigationBar(navController: NavHostController) {
 
     val navBackStackEntry by navController.currentBackStackEntryAsState()
     val currentRoute = navBackStackEntry?.destination?.route
-    val shouldShowBottomNavBar = items.any { it.route == currentRoute }
-    if(shouldShowBottomNavBar) {
+    var shouldShowBottomNavBar by rememberSaveable { (mutableStateOf(true)) }
+
+    LaunchedEffect(key1 = currentRoute) {
+        shouldShowBottomNavBar = items.any { it.route == currentRoute }
+    }
+
+    AnimatedVisibility(
+        visible = shouldShowBottomNavBar,
+        enter = slideInVertically(initialOffsetY = { it }),
+        exit = slideOutVertically(targetOffsetY = { it }),
+    ) {
         BottomNavBarContent(
             items = items,
             currentRoute = currentRoute,
