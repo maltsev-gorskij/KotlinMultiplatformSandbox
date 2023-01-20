a0e0667b301d7a88b4d784a60cdd3b1b64083c63
Dmitry Maltsev-Gorskij <maltsev-gorskij@yandex.ru>
2023-01-20 17:06:40 +0300

    - Added navigation routes for bottom nav

--- androidApp/src/main/java/ru/lyrian/kotlinmultiplatformsandbox/android/core/ui/navigation/NavigationRoot.kt
+++ androidApp/src/main/java/ru/lyrian/kotlinmultiplatformsandbox/android/core/ui/navigation/NavigationRoot.kt
@@ -1,9 +1,11 @@
 package ru.lyrian.kotlinmultiplatformsandbox.android.core.ui.navigation
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -18,7 +20,32 @@ fun NavigationRoot() {
         composable(NavigationDestinations.LAUNCHES_LIST.route) { LaunchesListScreen(navController) }
         composable(NavigationDestinations.LAUNCHES_DETAILS.route) {
             Surface(modifier = Modifier.fillMaxSize()) {
-                Text("Just a favorites placeholder")
+                Box {
+                    Text(
+                        modifier = Modifier.align(Alignment.Center),
+                        text = "Details placeholder"
+                    )
+                }
+            }
+        }
+        composable(NavigationDestinations.LAUNCHES_FAVORITES.route) {
+            Surface(modifier = Modifier.fillMaxSize()) {
+                Box {
+                    Text(
+                        modifier = Modifier.align(Alignment.Center),
+                        text = "Favorites placeholder"
+                    )
+                }
+            }
+        }
+        composable(NavigationDestinations.PROFILE.route) {
+            Surface(modifier = Modifier.fillMaxSize()) {
+                Box {
+                    Text(
+                        modifier = Modifier.align(Alignment.Center),
+                        text = "Profile placeholder"
+                    )
+                }
             }
         }
     }
